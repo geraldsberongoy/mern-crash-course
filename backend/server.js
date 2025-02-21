347313e4ca2a7da993d8817c3c67d6384525c09b
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-02-21 22:21:20 +0800

    feat: add logger middleware to server for enhanced request logging

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import logger from "./middleware/logger.js";
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ const app = express();
 
 app.use(express.json()); // allow us to accept JSON data in a body of a request
 app.use(express.urlencoded({ extended: false })); // allow us to accept form data
+
+// Middleware logger
+app.use(logger);
 
 app.use("/api/products", productRoutes);
 
