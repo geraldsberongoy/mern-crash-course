cf9fe03d47d024c4d4eca2b8a64940c422f569f1
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-02-21 21:49:55 +0800

    feat: add discount and updatedPrice fields to product model with pre-save middleware

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -14,11 +14,24 @@ const productSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    discount: {
+      type: Number,
+      required: false,
+    },
+    updatedPrice: { type: Number },
   },
   {
     timestamps: true, // this will add created at and updated at fields
   }
 );
+
+// Middleware to update the updatedPrice field
+productSchema.pre("save", function (next) {
+  this.updatedPrice = this.discount
+    ? this.price - (this.price * this.discount) / 100
+    : this.price;
+  next();
+});
 
 const Product = mongoose.model("Product", productSchema);
 
