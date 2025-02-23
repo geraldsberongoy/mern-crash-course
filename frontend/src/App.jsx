14b75aa8e789423a59c16a370071ab2d5d49f1e8
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-02-23 16:25:31 +0800

    Setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div className="flex justify-center items-center text-red-500">
+        testing
+      </div>
+    </div>
+  );
+}
+
+export default App;
