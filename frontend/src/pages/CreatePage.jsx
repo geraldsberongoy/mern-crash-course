2718d5276c38263a19f51fb733532078d775ef91
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-02-24 22:25:46 +0800

    feat: refactor CreatePage to use ProductForm component for product creation

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import AddAlert from "../components/Alert/AddAlert.jsx";
+import ProductForm from "../components/ProductForm.jsx";
 
 const CreatePage = () => {
   const [product, setProduct] = useState({
@@ -13,14 +14,6 @@ const CreatePage = () => {
   const [productName, setProductName] = useState("");
 
   const [alertVisible, setAlertVisible] = useState(false);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setProduct((prevProduct) => ({
-      ...prevProduct,
-      [name]: value,
-    }));
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -56,61 +49,11 @@ const CreatePage = () => {
       {alertVisible && (
         <AddAlert productName={productName} setProduct={setProduct} />
       )}
-      <form
-        onSubmit={handleSubmit}
-        className="fieldset bg-base-200 border-base-300 rounded-box h-auto w-xs border p-4"
-      >
-        <legend className="fieldset-legend justify-center">
-          <span className="text-center text-xl font-bold">Create Product </span>
-        </legend>
-
-        <label className="fieldset-label font-semibold">Name</label>
-        <input
-          type="text"
-          name="name"
-          className="input"
-          placeholder="Name"
-          value={product.name}
-          required={true}
-          onChange={handleChange}
-        />
-
-        <label className="fieldset-label font-semibold">Price</label>
-        <input
-          type="number"
-          name="price"
-          className="input"
-          placeholder="Price"
-          value={product.price}
-          required={true}
-          onChange={handleChange}
-        />
-
-        <label className="fieldset-label font-semibold">Discount</label>
-        <input
-          type="number"
-          name="discount"
-          className="input"
-          placeholder="Discount"
-          value={product.discount}
-          onChange={handleChange}
-        />
-
-        <label className="fieldset-label font-semibold">Image Link</label>
-        <input
-          type="text"
-          name="image"
-          className="input"
-          placeholder="Image Link"
-          value={product.image}
-          required={true}
-          onChange={handleChange}
-        />
-
-        <button type="submit" className="btn btn-success mt-4">
-          Create
-        </button>
-      </form>
+      <ProductForm
+        product={product}
+        setProduct={setProduct}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 };
