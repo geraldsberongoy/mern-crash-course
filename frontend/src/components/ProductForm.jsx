f122da9c8034d21016ea4cf275a59a54f1232233
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-02-24 22:25:41 +0800

    feat: add ProductForm component for creating and editing products

---
+++ frontend/src/components/ProductForm.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+const ProductForm = ({ product, setProduct, handleSubmit }) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProduct((prevProduct) => ({
+      ...prevProduct,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="fieldset bg-base-200 border-base-300 rounded-box h-auto w-xs border p-4"
+    >
+      <legend className="fieldset-legend justify-center">
+        <span className="text-center text-xl font-bold">
+          {product._id ? "Edit Product" : "Create Product"}
+        </span>
+      </legend>
+
+      <label className="fieldset-label font-semibold">Name</label>
+      <input
+        type="text"
+        name="name"
+        className="input"
+        placeholder="Name"
+        value={product.name}
+        required={true}
+        onChange={handleChange}
+      />
+
+      <label className="fieldset-label font-semibold">Price</label>
+      <input
+        type="number"
+        name="price"
+        className="input"
+        placeholder="Price"
+        value={product.price}
+        required={true}
+        onChange={handleChange}
+      />
+
+      <label className="fieldset-label font-semibold">Discount</label>
+      <input
+        type="number"
+        name="discount"
+        className="input"
+        placeholder="Discount"
+        value={product.discount}
+        onChange={handleChange}
+      />
+
+      <label className="fieldset-label font-semibold">Image Link</label>
+      <input
+        type="text"
+        name="image"
+        className="input"
+        placeholder="Image Link"
+        value={product.image}
+        required={true}
+        onChange={handleChange}
+      />
+
+      <button type="submit" className="btn btn-success mt-4">
+        {product._id ? "Save Changes" : "Create"}
+      </button>
+    </form>
+  );
+};
+
+export default ProductForm;
