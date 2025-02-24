7ee8b6422f5b639b301e209c73a9065f8a99965e
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-02-24 12:08:54 +0800

    feat: enhance NavBar with FontAwesome icons and theme controller integration

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,27 +1,24 @@
-// import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
+import ThemeController from "../ThemeController/ThemeController";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 flex-none shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link to={"/"}>
+          <p className="btn btn-ghost text-xl">PRODUCT STORE</p>
+        </Link>
       </div>
       <div className="flex-none">
+        <Link to="/create">
+          <button className="btn btn-square btn-ghost">
+            <FontAwesomeIcon size="2xl" icon={faSquarePlus} />
+          </button>
+        </Link>
         <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block h-5 w-5 stroke-current"
-          >
-            {" "}
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>{" "}
-          </svg>
+          <ThemeController />
         </button>
       </div>
     </div>
